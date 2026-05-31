06987f71b7b3966f48053dd60e26b9064bfb324b
aetheriastudios2-cmd <aetheriastudios2@gmail.com>
2026-06-01 00:10:55 +0100

    Create combat-system.m

---
+++ combat-system.m
@@ -0,0 +1,29 @@
+# Combat System
+
+## Melee
+
+- Light Attack
+- Heavy Attack
+- Block
+- Dodge
+- Parry
+
+## Magic
+
+- Fire
+- Ice
+- Lightning
+- Nature
+- Shadow
+
+## Ranged
+
+- Bow
+- Crossbow
+- Throwing Weapons
+
+## Future
+
+- Mounted Combat
+- Dragon Combat
+- Airship Combat
